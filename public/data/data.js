8c34fc6a5ab4c96013935a71f7d40a96ffac4613
kim jae hun <zg788990@gmail.com>
2020-10-19 18:44:04 +0900

    map search

--- public/data/data.js
+++ public/data/data.js
@@ -1,17 +1,24 @@
 //marker에 대한 데이터를 별도로 관리
+let days = new Date();
+const year = days.getFullYear();
+const month = days.getMonth() + 1;
+const day = days.getDate();
+
+let todayDate = `${year}.${month}.${day}`;
+
 var data = [
   //위치 정보 , 컨텐츠
   {
     title: '서울',
     content: '서울 남산',
-    date: '2020-10-17',
+    date: todayDate,
     lat: 37.552873,
     lng: 126.983544,
-  }, 
+  },
   {
     title: '서울',
     content: '서울 용산',
-    date: '2020-10-17',
+    date: todayDate,
     lat: 37.532768,
     lng: 126.990054,
   }
